349660739cf7673174ec1cf057884504c608263d
WhackyFool <harshitvidhyarthi@gmail.com>
2019-09-09 17:32:35 +0530

    Java assignments

--- ZemosoTrainingJava/Assignment6/src.main/java6.java
+++ ZemosoTrainingJava/Assignment6/src.main/java6.java
@@ -9,7 +9,8 @@ public class java6 {
     }
 
     private static boolean fangCheck(long orig, long fang1, long fang2){
-        if(Long.toString(fang1).endsWith("0") && Long.toString(fang2).endsWith("0")) return false;
+        if(Long.toString(fang1).endsWith("0") && Long.toString(fang2).endsWith("0"))
+            return false;
 
         int origLen = numDigits(orig);
         if(numDigits(fang1) != origLen / 2 || numDigits(fang2) != origLen / 2) return false;
@@ -24,23 +25,14 @@ public class java6 {
     public static void main(String[] args){
         HashSet<Long> vamps = new HashSet<Long>();
         for(long i = 10; vamps.size() <= 100; i++ ){
-            if((numDigits(i) % 2) != 0) {i = i * 10 - 1; continue;}
+            if((numDigits(i) % 2) != 0) {
+                i = i * 10 - 1; continue;
+            }
             for(long fang1 = 2; fang1 <= Math.sqrt(i) + 1; fang1++){
                 if(i % fang1 == 0){
                     long fang2 = i / fang1;
                     if(fangCheck(i, fang1, fang2) && fang1 <= fang2){
                         vamps.add(i);
-                        System.out.println(i + ": [" + fang1 + ", " + fang2 +"]");
-                    }
-                }
-            }
-        }
-        Long[] nums = {16758243290880L, 24959017348650L, 14593825548650L};
-        for(Long i : nums){
-            for(long fang1 = 2; fang1 <= Math.sqrt(i) + 1; fang1++){
-                if(i % fang1 == 0){
-                    long fang2 = i / fang1;
-                    if(fangCheck(i, fang1, fang2) && fang1 <= fang2){
                         System.out.println(i + ": [" + fang1 + ", " + fang2 +"]");
                     }
                 }
