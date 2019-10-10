9f1c6e953391aab7cac1e0f8af773f78fcb7833c
Harshit Vidhyarthi <45065771+whackyfool@users.noreply.github.com>
2019-10-10 11:13:23 +0530

    Added comments

--- ZemosoTrainingJava/Assignment5/src.main/Java5.java
+++ ZemosoTrainingJava/Assignment5/src.main/Java5.java
@@ -1,18 +1,29 @@
 package src.main;
 
+/*Create main class, create object of first class and call both the methods 
+to print the values. Create object of second class using static method and 
+then call the other method to print the String.*/
+
 public class Java5 {
     public static void main(String[] args) {
+        //Data object
         Data d = new Data();
         d.num = 5;
         d.ch = 'a';
         d.printInt();
         d.printChar();
         d.printLocal();
+        
+        //Singleton object
         Singleton s = new Singleton();
         s = s.setString("abc");
         s.printString();
     }
 }
+
+/*Create class singleton containing a non static String member variable.0
+Add a static method that takes String as parameter and initialize the member 
+variable and then return object of that class. Add a non static method to print the String.*/
 
 class Singleton {
     private String s;
@@ -26,6 +37,11 @@ class Singleton {
     }
 }
 
+/* Create a class data containing an int and a char member 
+variables that are not initialized, add a method to print 
+these variables. Add another method in the same class with 
+two local variables and print their values without initializing them.*/
+
 class Data {
     public int num;
     public char ch;
@@ -38,10 +54,11 @@ class Data {
     public void printLocal() {
         int i;
         char j;
-        // Variable i,j might not have been initialized.
-        // Default Constructor can only initialize data members
-        // It can't deal with local variables defined separately.
         //System.out.println(i);
         //System.out.println(j);
+        
+        /*Variable i,j might not have been initialized.
+        Default Constructor can only initialize data members
+        It can't deal with local variables defined separately.*/
     }
 }
